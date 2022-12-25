be53c44e33d8df7c4782eebf00097bd3eaa11c6f
nandikafajarr <nandikafajarr@gmail.com>
2022-12-25 22:56:17 +0700

    progres1

---
+++ script.js
@@ -0,0 +1,32 @@
+const menubar = document.querySelector(".menu-bar");
+const menunav = document.querySelector(".menu");
+menubar.addEventListener("click", () => {
+    menunav.classList.toggle('menu-active');
+});
+
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY)
+    const windowPosition = window.scrollY > 0;
+    navbar.classList.toggle('scrolling-active', windowPosition)
+})
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay: {
+        delay: 9000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
